fc485f25cdd616457e86fb0d26764383a13b1f02
heabijay <heabijay@gmail.com>
2021-02-28 19:34:35 +0200

    DAL: Removed Id property in profile model

--- PicoShelter-ApiServer.DAL/Entities/ProfileEntity.cs
+++ PicoShelter-ApiServer.DAL/Entities/ProfileEntity.cs
@@ -1,13 +1,19 @@
 ﻿using PicoShelter_ApiServer.DAL.Abstract;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace PicoShelter_ApiServer.DAL.Entities
 {
     public class ProfileEntity : Entity
     {
+        [NotMapped]
+        public new int Id { get => AccountId; }
         public string Firstname { get; set; }
         public string Lastname { get; set; }
 
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
         public int AccountId { get; set; }
         public virtual AccountEntity Account { get; set; }
 
