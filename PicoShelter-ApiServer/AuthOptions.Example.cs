b181046f7c5ca292581eda5c2b6ce1149cf8163a
heabijay <heabijay@gmail.com>
2021-05-25 22:30:01 +0300

    AuthOptions.Example replaces AuthOptions

---
+++ PicoShelter-ApiServer/AuthOptions.Example.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Text;
+
+namespace PicoShelter_ApiServer
+{
+    public static class AuthOptions
+    {
+        public const string Issuer = "PicoShelterServer";
+        public const string Audience = "PicoShelterClient";
+        const string Key = "**MyVeryStrongPassword**";
+        public static TimeSpan LifeTime = TimeSpan.FromDays(30);
+
+        public static SymmetricSecurityKey GetSymmetricSecurityKey()
+        {
+            return new SymmetricSecurityKey(Encoding.ASCII.GetBytes(Key));
+        }
+    }
+}
